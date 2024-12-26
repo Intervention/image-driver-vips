8225bea8a3a9c7a15d1a719cb1cc808e066e1213
Thomas <thomas@sctr.net>
2024-12-26 15:49:46 -0600

    scale down test

--- tests/Unit/Modifiers/ScaleDownModifierTest.php
+++ tests/Unit/Modifiers/ScaleDownModifierTest.php
@@ -4,21 +4,20 @@ declare(strict_types=1);
 
 namespace Intervention\Image\Drivers\Vips\Tests\Unit\Modifiers;
 
+use Intervention\Image\Drivers\Vips\Modifiers\ScaleDownModifier;
 use Intervention\Image\Drivers\Vips\Tests\BaseTestCase;
-use Intervention\Image\Modifiers\ScaleModifier;
 use PHPUnit\Framework\Attributes\CoversClass;
 
-#[CoversClass(ScaleModifier::class)]
-#[CoversClass(\Intervention\Image\Drivers\Vips\Modifiers\ScaleModifier::class)]
-final class ScaleModifierTest extends BaseTestCase
+#[CoversClass(ScaleDownModifier::class)]
+final class ScaleDownModifierTest extends BaseTestCase
 {
     public function testModify(): void
     {
         $image = $this->readTestImage('blocks.png');
         $this->assertEquals(640, $image->width());
         $this->assertEquals(480, $image->height());
-        $image->modify(new ScaleModifier(800));
-        $this->assertEquals(800, $image->width());
-        $this->assertEquals(600, $image->height());
+        $image->modify(new ScaleDownModifier(600, 600));
+        $this->assertEquals(600, $image->width());
+        $this->assertEquals(450, $image->height());
     }
 }
