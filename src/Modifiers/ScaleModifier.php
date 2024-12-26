4a033f3ed76ba365f73ba2bbdd3a201b66a7b74a
Thomas <thomas@sctr.net>
2024-12-26 15:51:15 -0600

    fixed doc

--- src/Modifiers/ScaleModifier.php
+++ src/Modifiers/ScaleModifier.php
@@ -9,6 +9,11 @@ use Intervention\Image\Interfaces\SizeInterface;
 
 class ScaleModifier extends ResizeModifier
 {
+    /**
+     * {@inheritdoc}
+     *
+     * @see ResizeModifier::getAdjustedSize()
+     */
     protected function getAdjustedSize(ImageInterface $image): SizeInterface
     {
         return $image->size()->scale($this->width, $this->height);
