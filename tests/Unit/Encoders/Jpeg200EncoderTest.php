f507572ecc4f1e72b37594cb723c55a9f251314b
Thomas <thomas@sctr.net>
2024-12-28 11:01:13 -0500

    jpeg2000 encoder

---
+++ tests/Unit/Encoders/Jpeg200EncoderTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Intervention\Image\Drivers\Vips\Tests\Unit\Encoders;
+
+use Intervention\Image\Drivers\Vips\Driver;
+use Intervention\Image\Drivers\Vips\Encoders\Jpeg2000Encoder;
+use Intervention\Image\Drivers\Vips\Tests\BaseTestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(Jpeg2000Encoder::class)]
+final class Jpeg200EncoderTest extends BaseTestCase
+{
+    public function testEncode(): void
+    {
+        $image = (new Driver())->createImage(3, 2);
+        $encoder = new Jpeg2000Encoder(75);
+        $encoder->setDriver(new Driver());
+        $result = $encoder->encode($image);
+        $this->assertMediaType('image/jp2', $result);
+        $this->assertEquals('image/jp2', $result->mimetype());
+    }
+}
