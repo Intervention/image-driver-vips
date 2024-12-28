28880b22a5ea08dbd575299bfbde48b77ede32c4
Thomas <thomas@sctr.net>
2024-12-28 09:54:11 -0600

    Make sure gif is resized correctly (#16)

--- tests/Unit/Modifiers/ResizeModifierTest.php
+++ tests/Unit/Modifiers/ResizeModifierTest.php
@@ -12,7 +12,7 @@ use PHPUnit\Framework\Attributes\CoversClass;
 #[CoversClass(\Intervention\Image\Drivers\Vips\Modifiers\ResizeModifier::class)]
 final class ResizeModifierTest extends BaseTestCase
 {
-    public function testModify(): void
+    public function testResize(): void
     {
         $image = $this->readTestImage('blocks.png');
         $this->assertEquals(640, $image->width());
@@ -21,5 +21,15 @@ final class ResizeModifierTest extends BaseTestCase
         $this->assertEquals(200, $image->width());
         $this->assertEquals(100, $image->height());
         $this->assertColor(255, 0, 0, 255, $image->pickColor(150, 70));
+    }
+
+    public function testResizeAnimated(): void
+    {
+        $image = $this->readTestImage('animation.gif');
+        $this->assertEquals(20, $image->width());
+        $this->assertEquals(15, $image->height());
+        $image->modify(new ResizeModifier(10, 10));
+        $this->assertEquals(10, $image->width());
+        $this->assertEquals(10, $image->height());
     }
 }
