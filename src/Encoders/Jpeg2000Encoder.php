ceb985c772be011a668ce55dbb3f2458b7d3340b
Thomas <thomas@sctr.net>
2024-12-29 02:17:13 -0600

    jpeg2000 encoder (#19)
    * jpeg2000 encoder
    
    * allow image/x-jp2-codestream
    
    * Refactor mime type detection
    
    * Add BaseTestCase::assertMediaTypeJpeg2000()
    
    * Add debug info
    
    * Include alternative Jpeg 2000 magic byte signature
    
    * Revert "Add debug info"
    
    This reverts commit 5a656d320b8eb5d56091356704780175e9d24137.
    
    * Reduce sample size
    
    * Refactor Jpeg200EncoderTest::class
    
    ---------
    
    Co-authored-by: Oliver Vogel <oliver@olivervogel.com>

---
+++ src/Encoders/Jpeg2000Encoder.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Intervention\Image\Drivers\Vips\Encoders;
+
+use Intervention\Image\EncodedImage;
+use Intervention\Image\Encoders\Jpeg2000Encoder as GenericJpeg2000Encoder;
+use Intervention\Image\Interfaces\ImageInterface;
+use Intervention\Image\Interfaces\SpecializedInterface;
+
+class Jpeg2000Encoder extends GenericJpeg2000Encoder implements SpecializedInterface
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @see EncoderInterface::function()
+     */
+    public function encode(ImageInterface $image): EncodedImage
+    {
+        $result = $image->core()->native()->writeToBuffer('.j2k', [
+            'Q' => $this->quality,
+            'strip' => true,
+            'lossless' => false,
+        ]);
+
+        return new EncodedImage($result, 'image/jp2');
+    }
+}
