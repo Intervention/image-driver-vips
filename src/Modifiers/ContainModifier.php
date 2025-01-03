5797b588a56f5f590807efee2ee47391ae840c42
Thomas <thomas@sctr.net>
2025-01-03 12:22:27 -0600

    contain modifier

---
+++ src/Modifiers/ContainModifier.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Intervention\Image\Drivers\Vips\Modifiers;
+
+use Intervention\Image\Colors\Rgb\Channels\Alpha;
+use Intervention\Image\Colors\Rgb\Channels\Blue;
+use Intervention\Image\Colors\Rgb\Channels\Green;
+use Intervention\Image\Colors\Rgb\Channels\Red;
+use Intervention\Image\Drivers\Vips\Traits\PositionToGravity;
+use Intervention\Image\Exceptions\RuntimeException;
+use Intervention\Image\Interfaces\ImageInterface;
+use Intervention\Image\Interfaces\SpecializedInterface;
+use Intervention\Image\Modifiers\ContainModifier as GenericContainModifier;
+use Jcupitt\Vips\Exception as VipsException;
+use Jcupitt\Vips\Extend;
+
+class ContainModifier extends GenericContainModifier implements SpecializedInterface
+{
+    use PositionToGravity;
+
+    /**
+     * {@inheritdoc}
+     *
+     * @throws RuntimeException|VipsException
+     * @see ModifierInterface::apply()
+     */
+    public function apply(ImageInterface $image): ImageInterface
+    {
+        $resize = $this->getResizeSize($image);
+        $bgColor = $this->driver()->handleInput($this->background);
+
+        $resized = $image->core()->native()->thumbnail_image($resize->width(), [
+            'height' => $resize->height(),
+            'no_rotate' => true,
+        ]);
+
+        if (!$image->core()->native()->hasAlpha()) {
+            $image->core()->setNative(
+                $image->core()->native()->bandjoin_const(255)
+            );
+        }
+
+        $image->core()->setNative(
+            $resized->gravity(
+                $this->positionToGravity($this->position),
+                $resize->width(),
+                $resize->height(),
+                [
+                    'extend' => Extend::BACKGROUND,
+                    'background' => [
+                        $bgColor->channel(Red::class)->value(),
+                        $bgColor->channel(Green::class)->value(),
+                        $bgColor->channel(Blue::class)->value(),
+                        $bgColor->channel(Alpha::class)->value(),
+                    ],
+                ]
+            )
+        );
+
+        return $image;
+    }
+}
---
+++ tests/Unit/Modifiers/ContainModifierTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Intervention\Image\Drivers\Vips\Tests\Unit\Modifiers;
+
+use Intervention\Image\Drivers\Vips\Tests\BaseTestCase;
+use Intervention\Image\Modifiers\ContainModifier;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(ContainModifier::class)]
+#[CoversClass(\Intervention\Image\Drivers\Vips\Modifiers\ContainModifier::class)]
+final class ContainModifierTest extends BaseTestCase
+{
+    public function testModifyContain(): void
+    {
+        $image = $this->readTestImage('blocks.png');
+        $this->assertEquals(640, $image->width());
+        $this->assertEquals(480, $image->height());
+        $image->modify(new ContainModifier(200, 100, 'ff0'));
+        $this->assertEquals(200, $image->width());
+        $this->assertEquals(100, $image->height());
+        $this->assertColor(255, 255, 0, 255, $image->pickColor(0, 0));
+        $this->assertColor(0, 0, 0, 0, $image->pickColor(140, 10));
+        $this->assertColor(255, 255, 0, 255, $image->pickColor(175, 10));
+    }
+
+    public function testModifyContainPosition(): void
+    {
+        $image = $this->readTestImage('blocks.png');
+        $this->assertEquals(640, $image->width());
+        $this->assertEquals(480, $image->height());
+        $image->modify(new ContainModifier(1000, 600, 'FF0000', 'left'));
+        $this->assertEquals(1000, $image->width());
+        $this->assertEquals(600, $image->height());
+        $this->assertColor(0, 0, 255, 255, $image->pickColor(0, 0));
+        $this->assertColor(0, 0, 0, 0, $image->pickColor(600, 10));
+        $this->assertColor(255, 0, 0, 255, $image->pickColor(900, 10));
+    }
+}
