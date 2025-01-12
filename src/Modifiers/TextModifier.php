6800c775a8539d5597cb85ff4d5da0145c09271c
Oliver Vogel <oliver@olivervogel.com>
2025-01-12 10:04:51 +0100

    Adjust TextModifier::class

--- src/Modifiers/TextModifier.php
+++ src/Modifiers/TextModifier.php
@@ -8,6 +8,7 @@ use Intervention\Image\Drivers\Vips\FontProcessor;
 use Intervention\Image\Interfaces\ImageInterface;
 use Intervention\Image\Interfaces\SpecializedInterface;
 use Intervention\Image\Modifiers\TextModifier as GenericTextModifier;
+use Jcupitt\Vips\BlendMode;
 
 class TextModifier extends GenericTextModifier implements SpecializedInterface
 {
@@ -23,11 +24,26 @@ class TextModifier extends GenericTextModifier implements SpecializedInterface
         $lines = $fontProcessor->textBlock($this->text, $this->font, $this->position);
 
         foreach ($lines as $line) {
+            // build vips image from text
             $text = $fontProcessor->textToVipsImage((string) $line, $this->font, $color);
+
+            // original line height from vips image
+            $height = $text->height;
+
+            // apply rotation
+            $text = match ($this->font->angle()) {
+                0 => $text,
+                90.0, -270.0 => $text->rot90(),
+                180.0, -180.0 => $text->rot180(),
+                -90.0, 270.0 => $text->rot270(),
+                default => $text->similarity(['angle' => $this->font->angle()]),
+            };
+
+            // place line on image
             $image->core()->setNative(
-                $image->core()->native()->composite($text, 'over', [
+                $image->core()->native()->composite($text, BlendMode::OVER, [
                     'x' => $line->position()->x(),
-                    'y' => $line->position()->y(),
+                    'y' => $line->position()->y() - $height,
                 ])
             );
         }
