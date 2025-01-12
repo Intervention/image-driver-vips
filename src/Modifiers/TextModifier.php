9b3b408f673076c72a37bde242eb4b4332ef84ed
Oliver Vogel <oliver@olivervogel.com>
2025-01-12 10:24:11 +0100

    Make TextModifier::class able to modify animated images

--- src/Modifiers/TextModifier.php
+++ src/Modifiers/TextModifier.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace Intervention\Image\Drivers\Vips\Modifiers;
 
+use Intervention\Image\Drivers\Vips\Core;
 use Intervention\Image\Drivers\Vips\FontProcessor;
+use Intervention\Image\Interfaces\FrameInterface;
 use Intervention\Image\Interfaces\ImageInterface;
 use Intervention\Image\Interfaces\SpecializedInterface;
 use Intervention\Image\Modifiers\TextModifier as GenericTextModifier;
 use Jcupitt\Vips\BlendMode;
+use Jcupitt\Vips\Image as VipsImage;
 
 class TextModifier extends GenericTextModifier implements SpecializedInterface
 {
@@ -39,15 +42,40 @@ class TextModifier extends GenericTextModifier implements SpecializedInterface
                 default => $text->similarity(['angle' => $this->font->angle()]),
             };
 
-            // place line on image
-            $image->core()->setNative(
-                $image->core()->native()->composite($text, BlendMode::OVER, [
-                    'x' => $line->position()->x(),
-                    'y' => $line->position()->y() - $height,
-                ])
-            );
+            if (!$image->isAnimated()) {
+                // place line on image
+                $modified = $this->placeTextOnFrame(
+                    $text,
+                    $image->core()->first(),
+                    $line->position()->x(),
+                    $line->position()->y() - $height,
+                )->native();
+            } else {
+                $frames = [];
+                foreach ($image as $frame) {
+                    $frames[] = $this->placeTextOnFrame(
+                        $text,
+                        $frame,
+                        $line->position()->x(),
+                        $line->position()->y() - $height,
+                    );
+                }
+
+                $modified = Core::replaceFrames($image->core()->native(), $frames);
+            }
         }
 
+        $image->core()->setNative($modified);
+
         return $image;
+    }
+
+    private function placeTextOnFrame(VipsImage $text, FrameInterface $frame, int $x, int $y): FrameInterface
+    {
+        $frame->setNative(
+            $frame->native()->composite($text, BlendMode::OVER, ['x' => $x, 'y' => $y])
+        );
+
+        return $frame;
     }
 }
